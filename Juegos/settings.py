5d6c1996aa1b1a33ed5f7d29f0c7be46d110a275
Nicolas Plaza <niplinig@espol.edu.ec>
2022-08-30 16:44:53 -0500

    Añadiendo static

--- Juegos/settings.py
+++ Juegos/settings.py
@@ -129,6 +129,8 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/juegos/2022-Backend-Juegos/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
