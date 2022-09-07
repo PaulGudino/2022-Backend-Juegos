eb38a73a62452b1b1d96278dd21b00dc99aa8cc5
Paul Gudiño <pgudino@espol.edu.ec>
2022-09-07 02:57:18 -0500

    Documentacion

--- Juegos/urls.py
+++ Juegos/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -19,9 +19,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('AppJuegos.api.urls')),
     path('api/', include('AppJuegos.api.routers')),
