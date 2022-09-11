1253cdacbc5512adc2219c9c70170971d438f848
Paul Gudiño <pgudino@espol.edu.ec>
2022-09-10 22:32:24 -0500

    Actualizacion

--- Juegos/settings.py
+++ Juegos/settings.py
@@ -168,7 +168,7 @@ CORS_ALLOW_METHODS = [
 AUTH_USER_MODEL = 'AppJuegos.User'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
