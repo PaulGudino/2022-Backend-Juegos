7f3525889ece30515dc6e5d0b104d77c920da17f
Paul Gudiño <pgudino@espol.edu.ec>
2022-09-15 21:42:00 -0500

    Filtrado

--- Juegos/settings.py
+++ Juegos/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = 'django-insecure-47=alantoxcgi6$@4&xv-mj&ifbcni=+(3dw9wl+^@i*gj4p&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['juegos.pythonanywhere.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juegos.pythonanywhere.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -41,9 +41,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # )
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 
 }
 
@@ -83,26 +83,26 @@ WSGI_APPLICATION = 'Juegos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'juegos$default',
-#         'USER': 'juegos',
-#         'PASSWORD': 'Root@123',
-#         'HOST': 'juegos.mysql.pythonanywhere-services.com',
-#         'PORT': '3306'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'juegos',
-        'USER': 'admin',
+        'NAME': 'juegos$default',
+        'USER': 'juegos',
         'PASSWORD': 'Root@123',
-        'HOST': 'localhost',
+        'HOST': 'juegos.mysql.pythonanywhere-services.com',
         'PORT': '3306'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'juegos',
+#         'USER': 'admin',
+#         'PASSWORD': 'Root@123',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
 
 
 # Password validation
