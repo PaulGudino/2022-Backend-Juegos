b9d9175036a30cd95cc2a7231a950df2ece917a5
Diego Rojas <Diego-27ro@outlook.com>
2022-10-08 16:01:45 -0500

    add changes for test

--- Juegos/settings.py
+++ Juegos/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = 'django-insecure-47=alantoxcgi6$@4&xv-mj&ifbcni=+(3dw9wl+^@i*gj4p&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['juegos.pythonanywhere.com']
-# ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['juegos.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -95,14 +95,14 @@ WSGI_APPLICATION = 'Juegos.wsgi.application'
 #    }
 #}
 
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'juegos',
-         'USER': 'admin',
-         'PASSWORD': 'Root@123',
-         'HOST': 'localhost',
-         'PORT': '3306'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'juegos',
+        'USER': 'admin',
+        'PASSWORD': 'Root@123',
+        'HOST': 'localhost',
+        'PORT': '3306'
      }
  }
 
