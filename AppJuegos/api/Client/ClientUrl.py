a097e80affb8d67ccb6d8afbfa3034a6ab8afe61
Paul Gudiño <pgudino@espol.edu.ec>
2022-11-16 00:41:37 -0500

    Avance

--- AppJuegos/api/Client/ClientUrl.py
+++ AppJuegos/api/Client/ClientUrl.py
@@ -2,9 +2,12 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 from AppJuegos.api.Client.ClientApiviews import (
     ClientViewSet,
+    ClientFilter
 )
 
 router = DefaultRouter()
 router.register(r'client', ClientViewSet, basename='client')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('clientfilter/', ClientFilter.as_view(), name='clientfilter'),
+]
