120ef831c7476badc72d7b8f35cac8d38b448f59
Paul Gudiño <pgudino@espol.edu.ec>
2022-11-17 17:01:31 -0500

    [#182727486]

--- AppJuegos/api/AwardCondition/AwardConditionApiviews.py
+++ AppJuegos/api/AwardCondition/AwardConditionApiviews.py
@@ -9,7 +9,16 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class AwardConditionViewSet(CRUDViewSet):
     serializer_class = AwardConditionSerializer
     queryset = AwardCondition.objects.all()
+
+class AwardConditionFilter(generics.ListAPIView):
+    serializer_class = AwardConditionSerializer
+    queryset = AwardCondition.objects.all()
+    filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
+    search_fields = ['id']
+    filterset_fields = ['is_active']
+    ordering_fields = ['start_date', 'end_date']
--- AppJuegos/api/AwardCondition/AwardConditionSerializers.py
+++ AppJuegos/api/AwardCondition/AwardConditionSerializers.py
@@ -4,6 +4,7 @@ from AppJuegos.models import (
     AwardCondition,
 )
 from django.utils import timezone
+from datetime import datetime
 
 class AwardConditionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,15 +12,17 @@ class AwardConditionSerializer(serializers.ModelSerializer):
         fields= '__all__'
 
     def validate_start_date(self, value):
-        if value < timezone.now().date():
+        if value < timezone.now():
             raise serializers.ValidationError("La fecha de inicio debe ser mayor a la fecha actual")
         return value
 
     def validate_end_date(self, value):
-        if value < timezone.now().date():
+        if value < timezone.now():
             raise serializers.ValidationError("La fecha de fin debe ser mayor a la fecha actual")
 
-        if value < self.initial_data['start_date']:
+
+        start_date = datetime.strptime(self.initial_data['start_date'].replace('T', ' '), '%Y-%m-%d %H:%M')
+        if value < start_date:
             raise serializers.ValidationError("La fecha de fin debe ser mayor a la fecha de inicio")
         return value
 
