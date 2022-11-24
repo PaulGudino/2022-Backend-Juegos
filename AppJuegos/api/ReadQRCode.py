5889691993ebab978985945202dc7413187f546a
Nicolás Andrés Plaza Iñiguez <niplinig@espol.edu.ec>
2022-11-24 11:11:40 -0500

    Changes to be committed: 	modified:   AppJuegos/api/GenerateQRCode.py 	modified:   AppJuegos/api/ReadQRCode.py 	deleted:    AppJuegos/api/qrcode.png 	modified:   requerimientos.txt  Untracked files: 	.idea/

--- AppJuegos/api/ReadQRCode.py
+++ AppJuegos/api/ReadQRCode.py
@@ -2,10 +2,23 @@
 # sudo apt-get install libzbar0
 
 from pyzbar import pyzbar
+from pathlib import Path
 from PIL import Image
 
-image = Image.open("qrcode.png")
-qr_code = pyzbar.decode(image)[0]
+def readCode(fileName):
 
-data = qr_code.data.decode('utf8').encode('shift-jis').decode('utf-8')
-print("The data is: ", data)
+    cwd = Path.cwd()
+    joined_path = cwd / "media"
+
+    if (Path(joined_path).exists()):
+        joined_path = joined_path / fileName
+        if (Path(joined_path).exists()):
+            image = Image.open(joined_path)
+            qr_code = pyzbar.decode(image)[0]
+            data = qr_code.data.decode('utf8').encode('shift-jis').decode('utf-8')
+            print("The data is: ", data)
+        else:
+            print("File not found")
+    else:
+        print("media folder not found")
+
